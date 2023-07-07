c1359ff58379f9beef7232af89b468b8d5de659f
brandonm98 <brandon_m98@live.com.mx>
2023-07-07 00:36:53 -0600

    update mega

--- arduino_mega/arduino_mega.ino
+++ arduino_mega/arduino_mega.ino
@@ -1,9 +1,3 @@
-#include "DHT.h"
-// IR sensor pin
-#define PIRPin 40
-#define IRPin 42
-// Alarm pin
-#define alarmPin 36
 // hopper sensor pin
 #define sensorPin 21
 #define sensorPin2 20
@@ -13,21 +7,13 @@
 #define motorPin 52
 #define motorPin2 50
 #define motorPin3 48
-// TMP sensor type and pin
-#define DHTPIN 38
-#define DHTTYPE DHT11
 
-DHT dht(DHTPIN, DHTTYPE);
-
-int motion;
-int pos;
 volatile int amountDispensed = 0;
 volatile int amountDispensed2 = 0;
 volatile int amountDispensed3 = 0;
 
 int flag = 0;  // CONT1
 int flag2 = 0; // CONT2
-int flag3 = 0; // ALARM
 int flag4 = 0; // SERIAL COM
 int flag5 = 0; // CONT3
 
@@ -35,84 +21,36 @@ int quantity;
 int quantity2;
 int quantity3;
 
-long t;
 long to_start;
 
 int ind1;
 int ind2;
 int ind3;
-String incomingString;
+
 byte tara;
+uint8_t bufPtr = 0;
 //////////////////////////////////////////////
-char buffer[100];
+char buffer[20];
+char receivedChars[20];
 //////////////////////////////////////////////
+void flush() {
+  bufPtr = 0;
+  for (uint8_t i = 0; i < 8; i++) {
+    receivedChars[i] = ' ';
+    buffer[i] = ' ';
+  }
+}
 void reset()
 {
   flag = 0;  // CONT1
   flag2 = 0; // CONT2
-  flag3 = 0; // ALARM
   flag4 = 0; // SERIAL COM
   flag5 = 0; // CONT3
   amountDispensed = 0;
   amountDispensed2 = 0;
   amountDispensed3 = 0;
   tara = 13;
-  incomingString = "";
-  delay(120);
-}
-/////////////////////////////////////////////
-void alarm()
-{
-  while (flag3 < 3)
-  {
-    digitalWrite(alarmPin, HIGH);
-    delay(350);
-    digitalWrite(alarmPin, LOW);
-    delay(500);
-    flag3++;
-  }
-  flag3 = 0;
-}
-////////////////////////////////////////////
-void sensores()
-{
-  if (millis() > t + 100)
-  {
-    float h = dht.readHumidity();
-    int temp = dht.readTemperature();
-    float f = dht.readTemperature(true);
-    if (isnan(t) || isnan(f))
-    {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
-    }
-    float hif = dht.computeHeatIndex(f, h);
-    float hic = dht.computeHeatIndex(temp, h, false);
-    if (temp > 30)
-    {
-      Serial.println("Overheating!");
-      alarm();
-    }
-    if (digitalRead(PIRPin) == HIGH)
-    {
-      motion = 1;
-      alarm();
-    }
-    else
-    {
-      motion = 0;
-    }
-    if (digitalRead(IRPin) == HIGH)
-    {
-      pos = 1;
-      alarm();
-    }
-    else
-    {
-      pos = 0;
-    }
-    t = millis();
-  }
+  flush();
 }
 ////////////////////////////////////////////////////////////////////////////////
 void coinPulse()
@@ -167,47 +105,42 @@ void setup()
   pinMode(motorPin, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
-  // alarm set up pin
-  pinMode(alarmPin, OUTPUT);
-  // IR sensor set up
-  pinMode(PIRPin, INPUT_PULLUP);
-  pinMode(IRPin, INPUT_PULLUP);
-
   // Trun off hoppers
   digitalWrite(motorPin, LOW);
   digitalWrite(motorPin2, LOW);
   digitalWrite(motorPin3, LOW);
 
-  // init temp sens
-  dht.begin();
+  while(!Serial){
+    ;
+  }
   Serial.println("Startup is complete");
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop()
 {
-  if (flag4 == 0)
-  {
-    sensores();
-  }
-
   ///////////////TARA
-
   if (Serial.available())
   {
     byte tara = Serial.read();
     if (tara >= 48 and tara <= 57)
     {
       flag4 = 1;
-      while (Serial.available() == 0){}
-      incomingString = char(tara) + Serial.readString();
-      incomingString.trim();
-      ind1 = incomingString.indexOf(',');
-      quantity = incomingString.substring(0, ind1).toInt();
-      ind2 = incomingString.indexOf(',', ind1 + 1);
-      quantity2 = incomingString.substring(ind1 + 1, ind2).toInt();
-      ind3 = incomingString.indexOf(',', ind2 + 1);
-      quantity3 = incomingString.substring(ind2 + 1, ind3).toInt();
-
+      bufPtr = 1;
+      byte rb_char;
+      receivedChars[0] = char(tara);
+      while(true){
+        rb_char = Serial.read();
+        if(rb_char != 255){
+          receivedChars[bufPtr] = char(rb_char);
+          bufPtr++;
+          if (bufPtr == 5){
+            break;
+          }
+        }
+      }
+      quantity = int(receivedChars[0])-48;
+      quantity2 = int(receivedChars[2])-48;
+      quantity3 = int(receivedChars[4])-48;
       // Give coins for each hopper
       to_start = millis();
       while (amountDispensed != quantity)
